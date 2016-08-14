cc851c875bf64d4686142c808bae667fa57a7abd
Chriz <cmcvista@gmail.com>
2016-08-14 23:27:29 +0800

    DeviceControlActivity: Added a textView. Need further amendment.

--- app/src/main/java/com/example/cmc7/sensationble/DeviceControlActivity.java
+++ app/src/main/java/com/example/cmc7/sensationble/DeviceControlActivity.java
@@ -2,12 +2,16 @@ package com.example.cmc7.sensationble;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DeviceControlActivity extends AppCompatActivity {
-
+    TextView debug_txtView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_device_control);
+        debug_txtView = (TextView) findViewById(R.id.textView);
+
+        //TOOD: Under Construction
     }
 }
